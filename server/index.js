1f1967d7290768a38951d50bdb2d76c17ec36d9e
sharith0811 <sharith.santos@estudiantesunibague.edu.co>
2026-04-30 17:53:54 -0500

    Websocket created

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {
+  console.log("Usuario conectado: " + socket.id);
+
+  socket.emit("message", "¡Bienvenido al chat!");
+
+  socket.on("message", (msg) => {
+    //io.emit => Envia a todos los clientes, incluido uno mismo
+    // socket.broadcast.emit => Envia a todos los clientes, excepto uno mismo
+    //socket.emit => Envia solo al cliente conectado
+
+    socket.emit("Confirmation", "Mensaje enviado");
+    socket.broadcast.emit("message", "Enviaron esto: " + msg);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo!");
+});
+
+server.listen(3000, () => {
+  console.log('Server corriendo en http://localhost:3000');
+});
