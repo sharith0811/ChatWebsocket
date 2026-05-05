c238c410ba53bc1473414f634f94f517dd02c37d
sharith0811 <sharith.santos@estudiantesunibague.edu.co>
2026-05-05 18:00:41 -0500

    cambios

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,21 +3,32 @@ import { io } from 'socket.io-client'
 
 function App() { 
   const[socket, setSocket] = useState()
-  
+  const[inputMessage, setInputMessage] = useState()
+
   //Actualizar el estado del socket cada vez que se conecta o desconecta
   useEffect(() => {
     const newSocket = io('localhost:3000')
-
+    setSocket(newSocket)
     return () => {
       newSocket.disconnect()
     }
   }, [])
 
+  const handleSubmit = () => {
+    //Enviar el mensaje al servidor a través del socket
+    e.preventDefault()
+    if(socket) {
+      socket.emit('message', inputMessage)
+    }
+  }
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Escribe un mensaje..." />
-        <button>Enviar</button>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Escribe un mensaje..." 
+        onChange={(e) => setInputMessage(e.target.value)}
+        />
+        <button onClick={handleSubmit}>Enviar</button>
       </form>
     </div>
   )
